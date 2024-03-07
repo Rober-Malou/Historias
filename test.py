2eae8d1ac109caa102f6566646d8136453e836fe
Roberto Benítez <benitezmartinroberto@gmail.com>
2024-03-07 05:24:28 +0100

    pequeño avance

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 import random
 
-# Plantilla personajes
-
 class Personaje:
     def __init__(self, nombre, sexo, especie, fuerza, inteligencia, carisma, suerte):
         self.nombre = nombre
@@ -21,12 +19,8 @@ class Personaje:
         else:
             ventaja == 0
 
-# Distintos personajes
-
 sandra = Personaje('Sandra', 'Mujer', 'Humana', 6, 8, 9, 5)
 
-# Opertura
-print('Bienvenida, ultimamente tu trabajo a sido muy duro, y el resto de tu vida tambien. No puedo hacer mucho, pero tampoco quiero no hacer nada. Asi que esto es tu opcion a una venganza dentro del marco de la ley. Espero que te guste y que perdones la infinidad de faltas de ortografia.')
+camello = sandra
 
-#Explicacion del juego
-print('Para avanzar en el juego tendras que escribir exactamente las palabras que aparezcan entre comillas ("Aqui") en la opcion que quieras escoger y pulsar "Enter"')
+print(vars(camello))
