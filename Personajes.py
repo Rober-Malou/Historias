f07e3a013685662c88700e33f3d2cf5f2d9fd156
Roberto <153698771+Rober-Malou@users.noreply.github.com>
2024-04-13 22:56:15 +0200

    avance con la salvacion del unicornio

--- Personajes.py
+++ Personajes.py
@@ -12,8 +12,11 @@ class Personaje:
         self.carisma = carisma
         self.suerte = suerte
         self.activo = activo
+
+# Funcion de Suerte
+
 def golpe_de_suerte(self):
-    milagro = random.randint(-6, 0)
+    milagro = random.randint(-7, 0)
     ventaja = milagro + self.suerte
     if ventaja >= 1:
         return(ventaja)
@@ -26,24 +29,31 @@ def golpe_de_suerte(self):
 sandra = Personaje('Sandra', 'Mujer', 'Humana', 6, 8, 7, 5, True)
 valeria = Personaje('Valeria', 'Mujer', 'Humana', 3, 6, 9, 6, False)
 
-Personajes_activos = [sandra]
+# Mensajes de Error
 
 fallo = 'Por el amor del dios tallarín, esa no es una de las opciones'
+
+# Variables de contador
 
 global choice1
 choice1 = 0
 
 # Obertura
-print('Bienvenida, últimamente tu trabajo a sido muy duro, y el resto de tu vida también. No puedo hacer mucho, pero tampoco quiero no hacer nada. Espero que te guste y que perdones la infinidad de faltas de ortografía.')
 
-#Explicación del juego
+print('Vienvenid@ espero que disfrutes esta corta historia interactiva y que su Tallarisca magnificencia te guie por el buen camino.')
+
+# Explicación del juego
+
 print('Para avanzar en el juego tendrás que escribir exactamente las palabras que aparezcan entre comillas ("Aquí") en la opción que quieras escoger (cuidado con acentos y esas cosas, soy un programador incompetente) y pulsar "Enter"')
 
-#Inicio de la aventura
+# Inicio de la aventura
+
 def inicio():
     print('Es una magnifica mañana de sábado en el reino. La reina Sandra se levanta en su castillo de la sierra y se asoma a su balcón a contemplar su reino. No es un reino demasiado grande, pero es de ella.\n Casi todo es bosque mágico, y salvo algún ocasional viajero o invitado no hay nadie en kilómetros a la redonda. Ahora que tu estas despierta.')
     decision1(choice1)
+
 #Primera decision del juego
+
 def decision1(choice1):
     if choice1 == 0:
         decision = str(input('¿Quieres ir a "despertar" a la princesa, "desayunar" o "salir" a dar el paseo? \n'))
@@ -76,19 +86,24 @@ def decision1(choice1):
             decision1(choice1)
     elif choice1 == 3:
         print('Ahora todo lo que queda es salir a dar un paseo.')
-        entrada_al_bosque()
+        paseo_con_valeria()
     else:
         print('Algo ha salido mal, el dios tallarín no me ha sonreído, en el gremio a esto lo llamamos bug.')
         inicio()
+
+# Escena del despertar
 
 def el_despertar_de_valeria():
     global choice1
     choice1 += 1
     print('Te diriges hacia la habitación de la princesa, una vez dentro te paras un instante a contemplar la escena. Valeria esta dulcemente dormida completamente tranquila. Su habitación esta decorada con unicornios y cosas monas.\n ')
     decision2()
+
+# Decision sobre como despertar a Valeria
+
 def decision2():
     global choice1
-    decision =str(input('¿Quieres, despertarla "dulcemente", "rápidamente" o dejarla "dormir"?\n'))
+    decision = str(input('¿Quieres, despertarla "dulcemente", "rápidamente" o dejarla "dormir"?\n'))
     if decision == 'dulcemente':
         print('Te acercas dulcemente hacia la cama y le acaricias con ternura la mejilla.')
         golpe_de_suerte(sandra)
@@ -118,6 +133,7 @@ def decision2():
         print(fallo)
         decision2()
 
+# Escena del Desayuno
 
 def el_desayuno():
     global choice1
@@ -129,8 +145,10 @@ def el_desayuno():
         print('Bajas a la cocina del castillo, allí encuentras un pollo con una sola ceja y un huevo crudo(solo la yema y la clara), ambos con gorros de cocinero y el pollo lleva la cascara como pantalones. \n')
         decision4()
 
+# Que Desayunar con Valeria
+
 def decision3 ():
-    decision= str(input('Shakipillo - ¿Que queréis desayunar hoy mamis? ¿Queréis "cereales", "bollos", o "huevos" con bacon?\n'))
+    decision = str(input('Shakipillo - ¿Que queréis desayunar hoy mamis? ¿Queréis "cereales", "bollos", o "huevos" con bacon?\n'))
     if decision == 'cereales':
         print('Gudetama - Que pereza.')
         print('Se ponen a preparar el desayuno a toda velocidad, en menos de 30 segundos todo esta listo y os lo sirven en el patio. \n Desayunáis tranquilamente, con el sol dándoos en la cara. Al terminar os disponéis a dar un tranquilo paseo por el bosque.')
@@ -162,8 +180,10 @@ def decision3 ():
         print(fallo)
         decision3()        
 
+# Que Desayunar Sola
+
 def decision4():
-    decision= str(input('Shakipillo - ¿Que quieres desayunar hoy mami? ¿Quieres "cereales", "bollos", o "huevos" con bacon?\n'))
+    decision = str(input('Shakipillo - ¿Que quieres desayunar hoy mami? ¿Quieres "cereales", "bollos", o "huevos" con bacon?\n'))
     if decision == 'cereales':
         print('Gudetama - Que pereza.')
         print('Se ponen a preparar el desayuno a toda velocidad, en menos de 30 segundos todo esta listo y te lo sirven en el patio. \n Desayunas tranquilamente, con el sol dándote en la cara.')
@@ -193,7 +213,9 @@ def decision4():
         decision1(choice1)
     else:
         print(fallo)
-        decision4()        
+        decision4()
+
+# Paseo con Valeria     
 
 def paseo_con_valeria():
     print('Empezáis a pasear, con el sol en la cara y el castillo a la espalda. Poco a poco os vais adentrando en el bosque, os encontráis ardillas escondiendo bellotas, pájaros sobrevolandoos.')
@@ -202,6 +224,32 @@ def paseo_con_valeria():
         print('Mientras te sobrevuela, un pájaro te bombardea. En silencio le das las das las gracias a su tallarinesca magnificencia porque te acordaste de llevar algo de agua y unos pañuelos de papel. Te limpias y sigues con tu paseo.')
     else:
         print('Ves como uno de los pájaros aligera la carga justo donde tu estas, pero con unos reflejos felinos lo esquivas en el ultimo momento y sigues tan tranquila con tu paseo.')
+    print('Mientras os adentráis en el bosque, ahora con un ojo en el cielo, vais tranquilamente charlando, disfrutando de la compañía de la otra. A lo lejos, un poco alejado del sendero veis un bulto extraño que no podéis identificar bien.')
+    vais_o_seguís()
+
+# Decision sobre si investigas el bulto o no
+    
+def vais_o_seguís():    
+    decision = str(input('Quieres, "acercarte" a ver o "seguir" con vuestro camino.\n'))
+    if decision == 'acercarte':
+        el_rescate_del_unicornio()
+    elif decision == 'seguir':
+        ascenso_a_la_montaña()
+    else:
+        print(fallo)
+        vais_o_seguís()
+
+# Rescate del unicornio
+
+def el_rescate_del_unicornio():
+    print('Os vais acercando cuidadosamente pero el bosque es demasiado denso y no podéis hacerlo muy deprisa.')
+    golpe_de_suerte(valeria)
+    if golpe_de_suerte == 0:
+        print('Valeria intenta pasar por encima de una rama, desafortunadamente se le engancha el segundo pie al intentar pasarlo por encima de la rama. Al caerse se raspa la rodilla y se hace sangre en la rodilla. ')
+
+     
+def ascenso_a_la_montaña():
+    print('culo')
 
 def entrada_al_bosque():
     print('pedo')
